ccc6d5ee7f7258684394f8a17b0d1e39efd9726d
garrettladley <ladley.g@northeastern.edu>
2023-09-21 10:18:33 -0400

    fixed cargo fmt err

--- api/src/main.rs
+++ api/src/main.rs
@@ -5,11 +5,7 @@ use turbine_api::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber(
-        "turbine_api".into(),
-        "info".into(),
-        std::io::stdout,
-    );
+    let subscriber = get_subscriber("turbine_api".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
