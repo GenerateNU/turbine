54f064bf2367195482643e55b65671d272e6083b
brianreicher <reicher.b@northeastern.edu>
2023-10-09 21:04:41 -0400

    MongoDB ingestion driver

--- ingestion/src/main.rs
+++ ingestion/src/main.rs
@@ -1,53 +1,16 @@
-use redis::{Client, Commands};
-use std::thread;
+use ingestion::mongo_utils;
 
-pub struct RedisDriver {
-    client: redis::Client,
-}
+async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("turbine_api".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
-impl RedisDriver {
-    pub fn new(host: &str, port: u16) -> Self {
-        let url = format!("redis://{}:{}", host, port);
-        let client = Client::open(url).expect("Failed to create Redis client");
-        RedisDriver { client }
-    }
+    let configuration = get_configuration().expect("Failed to read configuration.");
 
-    pub fn set(&self, key: &str, value: &str) -> redis::RedisResult<()> {
-        let mut conn = self.client.get_connection()?;
-        conn.set(key, value)?;
-        Ok(())
-    }
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
 
-    pub fn get(&self, key: &str) -> redis::RedisResult<Option<String>> {
-        let mut conn = self.client.get_connection()?;
-        let result: Option<String> = conn.get(key)?;
-        Ok(result)
-    }
-
-    pub fn delete(&self, key: &str) -> redis::RedisResult<()> {
-        let mut conn = self.client.get_connection()?;
-        conn.del(key)?;
-        Ok(())
-    }
-
-    pub fn run_queries_in_parallel(&self, keys: Vec<String>) -> Vec<redis::RedisResult<Option<String>>> {
-        let mut handles = vec![];
-
-        for key in keys {
-            let client = self.client.clone();
-            let handle = thread::spawn(move || {
-                let conn = client.get_connection();
-                conn.and_then(|mut conn| conn.get(&key))
-            });
-
-            handles.push(handle);
-        }
-
-        let mut results = vec![];
-        for handle in handles {
-            results.push(handle.join().unwrap());
-        }
-
-        results
-    }
-}
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await?;
+    O
---
+++ ingestion/src/mongo_utils.rs
@@ -0,0 +1,89 @@
+use mongodb::{Client, bson::Document, options::FindOptions};
+
+pub struct MongoDriver {
+    client: Option<Client>,
+    db_name: String,
+    host: String,
+    port: u16,
+}
+
+impl MongoDriver {
+    pub fn new(host: &str, port: u16, db_name: &str) -> Self {
+        MongoDriver {
+            db_name: db_name.to_string(),
+            host: host.to_string(),
+            port,
+            client: None, // Initialize client as None
+        }
+    }
+
+    pub async fn connect(&mut self) -> Result<(), mongodb::error::Error> {
+        let client = Client::with_uri_str(&format!("mongodb://{}:{}", self.host, self.port)).await?;
+        self.client = Some(client);
+        Ok(())
+    }
+
+    pub async fn disconnect(&mut self) {
+        self.client = None;
+    }
+
+    pub async fn flush_collection(&mut self, collection_name: &str) -> Result<(), mongodb::error::Error> {
+        if self.client.is_none() {
+            self.connect().await?;
+        }
+        let collection: mongodb::Collection<Document>= self.client.as_ref().unwrap().database(&self.db_name).collection(collection_name);
+        let result = collection.delete_many(Document::new(), None).await?;
+        println!("Deleted {} documents from {}", result.deleted_count, collection_name);
+        Ok(())
+    }
+    
+
+    pub async fn create_collection(&mut self, collection_name: &str) -> Result<(), mongodb::error::Error> {
+        if self.client.is_none() {
+            self.connect().await?;
+        }
+
+        self.client.as_ref().unwrap().database(&self.db_name).create_collection(collection_name, None).await?;
+        println!("Created collection {} in {}", collection_name, self.db_name);
+        Ok(())
+    }
+
+    pub async fn collection_size(&self, collection_name: &str) -> Result<u64, mongodb::error::Error> {
+        let collection: mongodb::Collection<Document>= self.client.as_ref().unwrap().database(&self.db_name).collection(collection_name);
+        let count = collection.count_documents(Document::new(), None).await?;
+        Ok(count)
+    }
+
+
+    pub async fn search_query(&self, collection_name: &str, qu: Document, proj: Document, lim: i64, show: bool) -> Result<Vec<Document>, mongodb::error::Error> {
+        let collection: mongodb::Collection<Document>= self.client.as_ref().unwrap().database(&self.db_name).collection(collection_name);
+        let mut options: FindOptions = FindOptions::default();
+        options.projection = Some(proj);
+        options.limit = Some(lim);
+        
+
+        let cursor: mongodb::Cursor<Document> = collection.find(qu, options).await?;
+        let mut result: Vec<Document> = Vec::new();
+        while let Some(doc) = cursor.next().await {
+            match doc {
+                Ok(document) => {
+                    if show {
+                        println!("{:?}", document);
+                    }
+                    result.push(document);
+                }
+                Err(e) => {
+                    eprintln!("Error: {:?}", e);
+                }
+            }
+        }
+
+        if show {
+            for document in &result {
+                println!("{:?}", document);
+            }
+        }
+    
+        Ok(result)
+    }
+}
