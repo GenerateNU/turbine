97f66cf93803a97490b930819124d7cd01e36ccf
brianreicher <reicher.b@northeastern.edu>
2023-11-18 17:06:05 -0500

    Gradient updater

---
+++ transformer/src/train/gradientupdate.py
@@ -0,0 +1,54 @@
+import functools
+import optax
+import jax
+import jax.numpy as np
+from typing import Mapping, Any
+
+
+class GradientUpdater:
+    """A stateless abstraction around an init_fn/update_fn pair.
+    This extracts some common boilerplate from the training loop.
+    """
+
+    def __init__(self, net_init, loss_fn,
+                 optimizer: optax.GradientTransformation):
+        self._net_init = net_init
+        self._loss_fn = loss_fn
+        self._opt = optimizer
+
+    @functools.partial(jax.jit, static_argnums=0)
+    def init(self, master_rng, data):
+        """Initializes state of the updater."""
+        out_rng, init_rng = jax.random.split(master_rng)
+        params = self._net_init(init_rng, data)
+        opt_state = self._opt.init(params)
+        out = dict(
+            step=np.array(0),
+            rng=out_rng,
+            opt_state=opt_state,
+            params=params,
+        )
+        return out
+
+    @functools.partial(jax.jit, static_argnums=0)
+    def update(self, state: Mapping[str, Any], data: Mapping[str, np.ndarray]) -> tuple:
+        """Updates the state using some data and returns metrics."""
+        rng, new_rng = jax.random.split(state['rng'])
+        params = state['params']
+        loss, g = jax.value_and_grad(self._loss_fn)(params, rng, data)
+
+        updates, opt_state = self._opt.update(g, state['opt_state'])
+        params = optax.apply_updates(params, updates)
+
+        new_state = {
+            'step': state['step'] + 1,
+            'rng': new_rng,
+            'opt_state': opt_state,
+            'params': params,
+        }
+
+        metrics = {
+            'step': state['step'],
+            'loss': loss,
+        }
+        return new_state, metrics
