c18183d1fdd9ece00e7a7acd2e6c3c1a511c3342
brianreicher <reicher.b@northeastern.edu>
2023-11-18 18:21:42 -0500

    Self supervised loss function

--- transformer/src/train/loss.py
+++ transformer/src/train/loss.py
@@ -8,14 +8,17 @@ def lm_loss_fn(forward_fn,
                params,
                rng,
                data: Mapping[str, np.ndarray],
-               is_training: bool = True) -> np.ndarray:
+               is_training: bool = True,
+               mask_prob: float = 0.15) -> np.ndarray:
     """Compute the loss on data wrt params."""
     logits = forward_fn(params, rng, data, is_training)
-    targets = jax.nn.one_hot(data['target'], vocab_size)
+    targets = data['obs']
     assert logits.shape == targets.shape
 
-    mask = np.greater(data['obs'], 0)
-    loss = -np.sum(targets * jax.nn.log_softmax(logits), axis=-1)
-    loss = np.sum(loss * mask) / np.sum(mask)
+    mask = (jax.random.uniform(rng, targets.shape) < mask_prob) & (targets > 0)
+    masked_targets = np.where(mask, targets, -1)  # Set non-masked positions to -1
+    masked_logits = np.where(mask, logits, 0.0)  # Zero out logits for non-masked positions
+    loss = jax.nn.softmax_cross_entropy_with_logits(masked_logits, jax.nn.one_hot(masked_targets, vocab_size))
+    loss = np.sum(loss) / np.sum(mask)
 
     return loss
